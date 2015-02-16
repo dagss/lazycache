33849c118a18b9fed43c67f8474808c3ca9af92e
Dag Sverre Seljebotn <d.s.seljebotn@astro.uio.no>
2015-02-16 12:21:28 +0100

    Change computation model to reflect direclty how user constructed tree

--- lazycache/lazy.py
+++ lazycache/lazy.py
@@ -1,3 +1,29 @@
+"""
+
+
+Choices made:
+
+* The lazy program built should, when evaluated, match very closely what
+  would have happened without caching. While user code should be 'pure',
+  other factors contribute too:
+
+  a) Code doing printout or other debug or non-debug side-effects can still
+     be considered "pure" for the purposes of computation. And it's good if
+     this follows the order it says in the tree-building code.
+
+  b) If one doesn't have enough memory for all temporary results, it's not
+     arbitrary which order things are computed in, and we have no model
+     or data to do a good job here and must rely on information implicitly
+     provided by the order of building the tree.
+
+  To ensure this we incrementally stamp each node on construction, and
+  always evaluate in order of that stamp. Obviously more options could be
+  added later..
+
+
+"""
+
+
 import operator
 import numpy as np
 import struct
@@ -46,12 +72,20 @@ def short_repr(x):
     return formatter(x)
 
 
+
+_global_expr_count = 0
+
+
 class BaseExpr(object):
-    pass
+    def __init__(self):
+        global _global_expr_count
+        self.stamp = _global_expr_count
+        _global_expr_count += 1
 
 
 class Expr(BaseExpr):
     def __init__(self, func_name, func_hash, func, args):
+        super(Expr, self).__init__()
         self.func_name = func_name
         self.func_hash = func_hash
         self.func = func
@@ -66,9 +100,17 @@ class Expr(BaseExpr):
         arg_strs = [arg.format(varnames) for arg in self.args]
         return '%s(%s)' % (self.func_name, ', '.join(arg_strs))
 
+    def format_single(self, arg_reprs):
+        return '%s(%s)' % (self.func_name, ', '.join(arg_reprs))
+
     def compute(self):
         computed_args = [arg.compute() for arg in self.args]
         return self.func(*computed_args)
+
+    def gather_program(self, exprs, leafs):
+        for arg in self.args:
+            arg.gather_program(exprs, leafs)
+        exprs.append(self)
 
 
 class InfixExpr(Expr):
@@ -76,9 +118,12 @@ class InfixExpr(Expr):
         arg_strs = [arg.format(varnames) for arg in self.args]
         return '(%s)' % (' %s ' % self.func_name).join(arg_strs)
 
+    def format_single(self, arg_reprs):
+        return '(%s)' % (' %s ' % self.func_name).join(arg_reprs)
 
 class Leaf(BaseExpr):
     def __init__(self, value, own):
+        super(Leaf, self).__init__()
         self.value = value
         self.immutable = is_immutable(value)
         self.own = own or self.immutable
@@ -87,7 +132,7 @@ class Leaf(BaseExpr):
     def compute(self):
         return self.value
 
-    def format(self, varnames):
+    def format(self, varnames, ):
         name = varnames.get(self, None)
         if name is None:
             if should_inline_repr(self.value):
@@ -99,44 +144,58 @@ class Leaf(BaseExpr):
     def serialize_for_hash(self, values):
         return values[self].hash
 
+    def gather_program(self, exprs, leafs):
+        leafs.append(self)
+
 
 def binop_impl(name, func):
     def method(self, other):
-        return Lazy(InfixExpr(name, name, func, (self.expr, lazy(other).expr)))
+        return Lazy(InfixExpr(name, name, func, (self._expr, lazy(other)._expr)))
     return method
 
 
 def rbinop_impl(name, func):
     def method(self, other):
-        return Lazy(InfixExpr(name, name, func, (lazy(other).expr, self.expr)))
+        return Lazy(InfixExpr(name, name, func, (lazy(other)._expr, self._expr)))
     return method
 
 
 class Lazy(object):
 
     def __init__(self, expr):
-        self.expr = expr
+        self._expr = expr
 
     def _secure_hash(self):
-        return self.expr.hash
+        return self._expr.hash
 
     def __compute__(self):
-        return self.expr.compute()
+        leaf_to_varname, statements = get_program(self._expr)
+        return evaluate_program(self._expr, leaf_to_varname, statements)
 
     def __repr__(self):
-        if isinstance(self.expr, Leaf):
-            return '<lazy %s %s>' % (hexlify(self.expr.hash[:3]), short_repr(self.expr.value))
+        if isinstance(self._expr, Leaf):
+            return '<lazy %s %s>' % (hexlify(self._expr.hash[:3]), short_repr(self._expr.value))
         else:
             hashval = self._secure_hash()
             varnames = {}
-            expr_repr = self.expr.format(varnames)
-            vars_reprs = []
-            leaf_and_name_list = varnames.items()
-            leaf_and_name_list.sort(key=lambda tup: tup[1])  # sort by varname
-            for leaf, name in leaf_and_name_list:
-                vars_reprs.append('  %s: %s %s' % (name, hexlify(leaf.hash[:3]), short_repr(leaf.value)))
-            return '<lazy {hash}\n\n  {expr}\n\nwith:\n\n{vars}\n)'.format(
-                hash=hexlify(hashval[:3]), expr=expr_repr, vars='\n'.join(vars_reprs))
+            leaf_to_varname, statements = get_program(self._expr)
+            lines = ['<lazy {hash}'.format(hash=hexlify(hashval[:3])), '  input:']
+            for varname, node in sorted([(name, leaf) for leaf, name in leaf_to_varname.items()]):
+                lines.append('    %s: %s %s' % (varname, hexlify(node.hash[:3]), short_repr(node.value)))
+            lines.extend(['  program:'])
+            for varname, node, arg_reprs in statements:
+                lines.append('    %s: %s %s' % (varname, hexlify(node.hash[:3]), node.format_single(arg_reprs)))
+            lines.append('>')
+            return '\n'.join(lines)
+            
+            #expr_repr = self._expr.format(varnames)
+            #vars_reprs = []
+            #leaf_and_name_list = varnames.items()
+            #leaf_and_name_list.sort(key=lambda tup: tup[1])  # sort by varname
+            #for leaf, name in leaf_and_name_list:
+            #    vars_reprs.append('  %s: %s %s' % (name, hexlify(leaf.hash[:3]), short_repr(leaf.value)))
+            #return '  {expr}\n\nwith:\n\n{vars}\n)'.format(
+            #    hash=hexlify(hashval[:3]), expr=expr_repr, vars='\n'.join(vars_reprs))
 
     __add__ = binop_impl('+', operator.add)
     __sub__ = binop_impl('-', operator.sub)
@@ -147,6 +206,47 @@ class Lazy(object):
     __rsub__ = rbinop_impl('-', operator.sub)
     __rmul__ = rbinop_impl('*', operator.mul)
     __rdiv__ = rbinop_impl('/', operator.div)
+
+
+def get_program(root):
+    def format_arg(arg):
+        return node_to_varname.get(arg, None) or repr(arg.value)
+            
+    exprs, leafs = [], []
+    root.gather_program(exprs, leafs)
+    leafs.sort(key=lambda x: x.stamp)
+    exprs.sort(key=lambda x: x.stamp)
+
+    leaf_to_varname = {}
+    node_to_varname = {}
+    idx = 0
+    for leaf in leafs:
+        if leaf not in leaf_to_varname and not should_inline_repr(leaf.value):
+            leaf_to_varname[leaf] = node_to_varname[leaf] = 'v%d' % idx
+            idx += 1
+
+    statements = []  #  (target, exprnode, (argname1, argname2, ...))
+    idx = 0
+    for expr in exprs:
+        if expr not in node_to_varname:
+            node_to_varname[expr] = varname = 'e%d' % idx
+            statements.append((varname, expr, tuple(format_arg(arg) for arg in expr.args)))
+            idx += 1
+
+    return leaf_to_varname, statements
+
+
+def evaluate_program(root, leaf_to_varname, statements):
+    vars = {}
+    for target, exprnode, argnames in statements:
+        args = tuple(arg.value if isinstance(arg, Leaf) else vars[argname]
+                     for argname, arg in zip(argnames, exprnode.args))
+        result = exprnode.func(*args)
+        if exprnode is root:
+            return result
+        else:
+            vars[target] = result
+    raise AssertionError()
 
 
 def lazy(value, own=False):
--- lazycache/tests/test_lazy.py
+++ lazycache/tests/test_lazy.py
@@ -1,27 +1,12 @@
 from binascii import hexlify
 from textwrap import dedent
 import numpy as np
-from ..lazy import lazy, compute
+from ..lazy import lazy, compute, get_program
 
 
 x = lazy(np.ones(3), own=True)
 y = lazy(np.ones(3), own=True)
 
-
-def test_hash_and_repr():
-    assert repr(x) == "<lazy 31f889 ndarray(shape=(3,), dtype=float64)>"
-    assert hexlify(x._secure_hash()) == '31f88943ab92191a951a50ab89631ffdf47e55a663671b30414b154636fa7e64'
-    assert repr((x + y) * 4) == dedent("""\
-        <lazy 165062
-
-          ((v0 + v1) * 4)
-
-        with:
-
-          v0: 31f889 ndarray(shape=(3,), dtype=float64)
-          v1: 31f889 ndarray(shape=(3,), dtype=float64)
-        )""")
-    assert hexlify(((x + y) * 4)._secure_hash()) == '165062018b6da74c73beb9ef15bda205ddfbf75d8263516e26d5d38328885af5'
 
 
 def test_compute():
@@ -29,3 +14,38 @@ def test_compute():
     e = x - x + (4 / (x + x)) * x
     v = compute(e)
     assert np.all(v == 2) and v.shape == (3,)
+
+
+def test_program_tracks_node_creation():
+    # Make sure that the way things are computed, and printed in repr, directly
+    # reflects exactly how the tree was constructed. So "e = 5 * x; e + e" has
+    # the same hash, but not the same program, as (5 * x) + (5 * x).
+    e = 5 * x
+    assert repr(e + e) == dedent("""\
+        <lazy 535687
+          input:
+            v0: 31f889 ndarray(shape=(3,), dtype=float64)
+          program:
+            e0: ddc9e1 (5 * v0)
+            e1: 535687 (e0 + e0)
+        >""")
+    v1 = compute(e + e)
+
+    assert repr((5 * x) + (5 * x)) == dedent("""\
+        <lazy 535687
+          input:
+            v0: 31f889 ndarray(shape=(3,), dtype=float64)
+          program:
+            e0: ddc9e1 (5 * v0)
+            e1: ddc9e1 (5 * v0)
+            e2: 535687 (e0 + e1)
+        >""")
+    v2 = compute((5 * x) + (5 * x))
+    
+    assert np.all(v1 == v2) and np.all(v2 == 10) and v1.shape == v2.shape == (3,)
+
+
+
+
+# TODO:
+# Lazy's hash and eq. e + e != (5 * x) + (5 * x)
